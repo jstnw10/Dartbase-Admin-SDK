f25f4159731a0a8ad75602fb1d7339f0515dd2c4
Demoniaque <saadodi44@gmail.com>
2020-06-01 10:52:23 +0300

    replace outdated duplicate _encode/_decode methods with firestore_encoding.dart class

--- lib/firestore/models.dart
+++ lib/firestore/models.dart
@@ -2,12 +2,8 @@ import 'dart:collection';
 
 import 'package:dartbase/generated/google/firestore/v1/document.pb.dart' as fs;
 import 'package:dartbase/generated/google/firestore/v1/query.pb.dart';
-import 'package:dartbase/generated/google/protobuf/struct.pb.dart';
-import 'package:dartbase/generated/google/protobuf/struct.pbenum.dart';
-import 'package:dartbase/generated/google/protobuf/timestamp.pb.dart';
 import 'package:dartbase/generated/google/protobuf/wrappers.pb.dart';
 import 'package:dartbase/generated/google/type/latlng.pb.dart';
-import 'package:fixnum/fixnum.dart';
 import 'package:grpc/grpc.dart';
 
 import 'firestore_encoding.dart';
@@ -45,7 +41,7 @@ abstract class Reference {
   fs.Document _encodeMap(Map<String, dynamic> map) {
     var document = fs.Document();
     map.forEach((key, value) {
-      document.fields[key] = _encode(value);
+      document.fields[key] = FirestoreEncoding.encode(value);
     });
     return document;
   }
@@ -189,7 +185,7 @@ class Document {
 
   dynamic operator [](String key) {
     if (!_rawDocument.fields.containsKey(key)) return null;
-    return _decode(_rawDocument.fields[key], _gateway);
+    return FirestoreEncoding.decode(_rawDocument.fields[key], _gateway);
   }
 
   @override
@@ -377,78 +373,5 @@ class QueryReference extends Reference {
   /// Delegates encoding the given [value] to [FirebaseEncoding.encode].
   fs.Value _encode(dynamic value) {
     return FirestoreEncoding.encode(value);
-  }
-}
-
-fs.Value _encode(dynamic value) {
-  if (value == null) return fs.Value()..nullValue = NullValue.NULL_VALUE;
-
-  var type = value.runtimeType;
-
-  if (type.toString().startsWith('List')) {
-    var array = fs.ArrayValue();
-    (value as List).forEach((element) => array.values.add(_encode(element)));
-    return fs.Value()..arrayValue = array;
-  }
-
-  if (type.toString().contains('Map')) {
-    var map = fs.MapValue();
-    (value as Map).forEach((key, val) => map.fields[key] = _encode(val));
-    return fs.Value()..mapValue = map;
-  }
-
-  if (type.toString() == 'Uint8List') {
-    return fs.Value()..bytesValue = value;
-  }
-
-  switch (type) {
-    case bool:
-      return fs.Value()..booleanValue = value;
-    case int:
-      return fs.Value()..integerValue = Int64(value);
-    case double:
-      return fs.Value()..doubleValue = value;
-    case DateTime:
-      return fs.Value()..timestampValue = Timestamp.fromDateTime(value);
-    case String:
-      return fs.Value()..stringValue = value;
-    case DocumentReference:
-      return fs.Value()..referenceValue = value._fullPath;
-    case GeoPoint:
-      return fs.Value()..geoPointValue = (value as GeoPoint).toLatLng();
-    default:
-      throw Exception('Unknown type: ${type}');
-  }
-}
-
-dynamic _decode(fs.Value value, FirestoreGateway gateway) {
-  switch (value.whichValueType()) {
-    case fs.Value_ValueType.nullValue:
-      return null;
-    case fs.Value_ValueType.booleanValue:
-      return value.booleanValue;
-    case fs.Value_ValueType.doubleValue:
-      return value.doubleValue;
-    case fs.Value_ValueType.stringValue:
-      return value.stringValue;
-    case fs.Value_ValueType.integerValue:
-      return value.integerValue.toInt();
-    case fs.Value_ValueType.timestampValue:
-      return value.timestampValue.toDateTime().toLocal();
-    case fs.Value_ValueType.bytesValue:
-      return value.bytesValue;
-    case fs.Value_ValueType.referenceValue:
-      return DocumentReference(gateway, value.referenceValue);
-    case fs.Value_ValueType.geoPointValue:
-      return GeoPoint.fromLatLng(value.geoPointValue);
-    case fs.Value_ValueType.arrayValue:
-      return value.arrayValue.values
-          .map((item) => _decode(item, gateway))
-          .toList(growable: false);
-    case fs.Value_ValueType.mapValue:
-      return value.mapValue.fields
-          .map((key, value) => MapEntry(key, _decode(value, gateway)));
-    default:
-      throw Exception('Unrecognized type: ${value}');
   }
 }
