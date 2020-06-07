aebf2780284950ccf6e5975603dc3214df4b7b36
Demoniaque <saadodi44@gmail.com>
2020-06-07 11:32:53 +0300

    add gcloud storage to export list

--- lib/dartbase.dart
+++ lib/dartbase.dart
@@ -5,3 +5,4 @@ export 'package:dartbase/base/firebase.dart';
 export 'package:dartbase/firestore/firestore.dart';
 export 'package:dartbase/firestore/models.dart';
 export 'package:dartbase/storage/firebase_storage.dart';
+export 'package:gcloud/storage.dart' hide Page;
